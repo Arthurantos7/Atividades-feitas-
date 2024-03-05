7e43f7c4ac300284d4df55fce0eb5807ef076ee9
Arthurantos7 <160605342+Arthurantos7@users.noreply.github.com>
2024-03-05 09:22:01 -0300

    Add files via upload

---
+++ Soma das notas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+void cabecalho(){
+	system("cls || clear");
+	printf("\n=== ESCOLA ===\n");
+	fflush(stdin);
+}
+	int main() {
+	setlocale(LC_ALL, "");
+	int nota, soma, i;
+	for (i = 1; i <= 5; i++){
+	
+	//ESCREVENDO DADOS
+	printf("Digite a %iª nota: ", i);
+	scanf("%i",&nota);soma = soma + nota;
+	
+	cabecalho();
+	printf("\nSoma: %i \n", soma);
+
+}
+	
+
+}
