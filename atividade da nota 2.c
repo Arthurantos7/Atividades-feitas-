15e158f217f9f8ae0153f929f24e819322fdd2e5
Arthurantos7 <160605342+Arthurantos7@users.noreply.github.com>
2024-03-12 08:43:38 -0300

    Add files via upload

---
+++ atividade da nota 2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main (){
+	setlocale(LC_ALL, "");
+	
+	//Declarando variáveis 
+	int nota, soma,  i;
+	float media;
+	
+	// Solicitando dados para o usuário.
+	for (i = i; i <= 45; i++){
+	
+		printf("Digite a %iª nota: ", i);
+		scanf("%i", &nota);	
+	
+	// Soma = soma + nota 
+	soma += nota;
+}
+	media = soma / 4;
+	
+	// Exibindo resultados
+	printf("\nMédia: %.1f \n", media);
+	
+	return 0;
+
+}
+
